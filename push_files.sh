f5091ee939c95fd911dbc1186ed7cce5c536575e
Poul Kalff <poulkalff@gmail.com>
2019-01-04 13:04:09 +0100

    changed from-paths to absolute

--- push_files.sh
+++ push_files.sh
@@ -7,18 +7,18 @@ if [ -z "$1"  ]; then                   # if nothing selected
         exit
 else
         echo '  Copying files....'
-        sudo cp modified_files/advancedsettings.xml             /home/$1/.kodi/userdata/
-        sudo cp modified_files/sources.xml                      /home/$1/.kodi/userdata/
-        sudo cp modified_files/*.xsp                            /home/$1/.kodi/userdata/playlists/video/
-        sudo cp modified_files/settings.xml                     /home/$1/.kodi/userdata/
-        sudo cp modified_files/*.png                            /home/$1/.kodi/addons/skin.confluence/media/
-        sudo cp modified_files/Home.xml                         /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp modified_files/IncludesHomeMenuItems.xml        /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp modified_files/VideoFullScreen.xml              /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp modified_files/DialogSeekBar.xml                /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/ConfluenceMod/modified_files/advancedsettings.xml             /home/$1/.kodi/userdata/
+        sudo cp /home/$1/ConfluenceMod/modified_files/sources.xml                      /home/$1/.kodi/userdata/
+        sudo cp /home/$1/ConfluenceMod/modified_files/*.xsp                            /home/$1/.kodi/userdata/playlists/video/
+        sudo cp /home/$1/ConfluenceMod/modified_files/settings.xml                     /home/$1/.kodi/userdata/
+        sudo cp /home/$1/ConfluenceMod/modified_files/*.png                            /home/$1/.kodi/addons/skin.confluence/media/
+        sudo cp /home/$1/ConfluenceMod/modified_files/Home.xml                         /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/ConfluenceMod/modified_files/IncludesHomeMenuItems.xml        /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/ConfluenceMod/modified_files/VideoFullScreen.xml              /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/ConfluenceMod/modified_files/DialogSeekBar.xml                /home/$1/.kodi/addons/skin.confluence/720p/
         if [ -d "/home/$1/.kodi/addons/plugin.video.dr.dk.bonanza" ]
         then
-                sudo cp modified_files/addon.py                 /home/$1/.kodi/addons/plugin.video.dr.dk.bonanza/
+                sudo cp /home/$1/ConfluenceMod/modified_files/addon.py                 /home/$1/.kodi/addons/plugin.video.dr.dk.bonanza/
         else
                 echo '   ("plugin.video.dr.dk.bonanza" does not exist, and was ignored!)'
         fi
