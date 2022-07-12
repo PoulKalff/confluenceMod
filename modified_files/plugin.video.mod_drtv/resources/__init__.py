a1a8b3eda1f6beae257aa5425391434db07fb9f1
poulkalff <poulkalff@gmail.com>
2022-07-12 23:03:00 +0200

    added 2 plugins

--- modified_files/plugin.video.mod_drtv/resources/__init__.py
+++
@@ -1,1 +0,0 @@
-__author__ = 'PrinceOfCats'
--- push_files.sh
+++ push_files.sh
@@ -7,18 +7,19 @@ if [ -z "$1"  ]; then                   # if nothing selected
         exit
 else
         echo '  Copying files....'
-        sudo cp /home/$1/confluenceMod/modified_files/advancedsettings.xml             /home/$1/.kodi/userdata/
-        sudo cp /home/$1/confluenceMod/modified_files/sources.xml                      /home/$1/.kodi/userdata/
-        sudo cp /home/$1/confluenceMod/modified_files/*.xsp                            /home/$1/.kodi/userdata/playlists/video/
-        sudo cp /home/$1/confluenceMod/modified_files/settings.xml                     /home/$1/.kodi/userdata/
-        sudo cp /home/$1/confluenceMod/modified_files/*.png                            /home/$1/.kodi/addons/skin.confluence/media/
-        sudo cp /home/$1/confluenceMod/modified_files/Home.xml                         /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp /home/$1/confluenceMod/modified_files/IncludesHomeMenuItems.xml        /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp /home/$1/confluenceMod/modified_files/IncludesBackgroundBuilding.xml   /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp /home/$1/confluenceMod/modified_files/Includes.xml                     /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp /home/$1/confluenceMod/modified_files/VideoFullScreen.xml              /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp /home/$1/confluenceMod/modified_files/DialogSeekBar.xml                /home/$1/.kodi/addons/skin.confluence/720p/
-        sudo cp -r /home/$1/confluenceMod/modified_files/plugin.video.mod_drtv         /home/$1/.kodi/addons/
+        sudo cp /home/$1/confluenceMod/modified_files/advancedsettings.xml                /home/$1/.kodi/userdata/
+        sudo cp /home/$1/confluenceMod/modified_files/sources.xml                         /home/$1/.kodi/userdata/
+        sudo cp /home/$1/confluenceMod/modified_files/*.xsp                               /home/$1/.kodi/userdata/playlists/video/
+        sudo cp /home/$1/confluenceMod/modified_files/settings.xml                        /home/$1/.kodi/userdata/
+        sudo cp /home/$1/confluenceMod/modified_files/*.png                               /home/$1/.kodi/addons/skin.confluence/media/
+        sudo cp /home/$1/confluenceMod/modified_files/Home.xml                            /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/confluenceMod/modified_files/IncludesHomeMenuItems.xml           /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/confluenceMod/modified_files/IncludesBackgroundBuilding.xml      /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/confluenceMod/modified_files/Includes.xml                        /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/confluenceMod/modified_files/VideoFullScreen.xml                 /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp /home/$1/confluenceMod/modified_files/DialogSeekBar.xml                   /home/$1/.kodi/addons/skin.confluence/720p/
+        sudo cp -r /home/$1/confluenceMod/modified_files/plugin.video.drnu                /home/$1/.kodi/addons/
+        sudo cp -r /home/$1/confluenceMod/modified_files/service.subtitles.opensubtitles  /home/$1/.kodi/addons/
         echo '     Done!'
         echo
 fi
