335892e2d81c564cba814238d9717cb141595a09
poulkalff <poulkalff@gmail.com>
2024-10-25 08:48:26 +0200

    update to 1080 V

--- push_files.sh
+++ push_files.sh
@@ -8,8 +8,6 @@ if [ -z "$1"  ]; then                   # if nothing selected
 else
         echo '  Fix permissions....'
 	sudo chmod 777 /home/$1/confluenceMod -R
-        echo '  Fetching Confluence skin....'
-	git clone https://github.com/xbmc/skin.confluence /home/$1/.kodi/addons
         echo '  Copying files....'
         sudo cp /home/$1/confluenceMod/modified_files/Home.xml                            /home/$1/.kodi/addons/skin.confluence/1080p/
         sudo cp /home/$1/confluenceMod/modified_files/IncludesHomeMenuItems.xml           /home/$1/.kodi/addons/skin.confluence/1080p/
@@ -24,6 +22,8 @@ else
         sudo cp /home/$1/confluenceMod/modified_files/*.png                               /home/$1/.kodi/addons/skin.confluence/media/
         sudo cp -r /home/$1/confluenceMod/modified_files/plugin.video.drnu                /home/$1/.kodi/addons/
         sudo cp -r /home/$1/confluenceMod/modified_files/service.subtitles.opensubtitles  /home/$1/.kodi/addons/
+        echo '  Fetching Confluence skin....'
+	git clone https://github.com/xbmc/skin.confluence /home/$1/.kodi/addons
         echo '  Fetching DR Addon....'
 	git clone https://github.com/xbmc-danish-addons/plugin.video.drnu /home/$1/.kodi/addons
         echo '     Done!'
