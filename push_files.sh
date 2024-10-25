a10f0e890fcbe5d837fb174b74f146f46c0fb107
poulkalff <poulkalff@gmail.com>
2024-10-25 08:47:10 +0200

    update to 1080 IV

--- push_files.sh
+++ push_files.sh
@@ -13,10 +13,10 @@ else
         echo '  Copying files....'
         sudo cp /home/$1/confluenceMod/modified_files/Home.xml                            /home/$1/.kodi/addons/skin.confluence/1080p/
         sudo cp /home/$1/confluenceMod/modified_files/IncludesHomeMenuItems.xml           /home/$1/.kodi/addons/skin.confluence/1080p/
-  #      sudo cp /home/$1/confluenceMod/modified_files/IncludesBackgroundBuilding.xml      /home/$1/.kodi/addons/skin.confluence/1080p/
+        sudo cp /home/$1/confluenceMod/modified_files/IncludesBackgroundBuilding.xml      /home/$1/.kodi/addons/skin.confluence/1080p/
+        sudo cp /home/$1/confluenceMod/modified_files/DialogSeekBar.xml                   /home/$1/.kodi/addons/skin.confluence/1080p/
+        sudo cp /home/$1/confluenceMod/modified_files/VideoFullScreen.xml                 /home/$1/.kodi/addons/skin.confluence/1080p/
 #        sudo cp /home/$1/confluenceMod/modified_files/Includes.xml                        /home/$1/.kodi/addons/skin.confluence/1080p/
-   #     sudo cp /home/$1/confluenceMod/modified_files/VideoFullScreen.xml                 /home/$1/.kodi/addons/skin.confluence/1080p/
-    #    sudo cp /home/$1/confluenceMod/modified_files/DialogSeekBar.xml                   /home/$1/.kodi/addons/skin.confluence/1080p/
         sudo cp /home/$1/confluenceMod/modified_files/advancedsettings.xml                /home/$1/.kodi/userdata/
         sudo cp /home/$1/confluenceMod/modified_files/sources.xml                         /home/$1/.kodi/userdata/
         sudo cp /home/$1/confluenceMod/modified_files/*.xsp                               /home/$1/.kodi/userdata/playlists/video/
@@ -24,6 +24,8 @@ else
         sudo cp /home/$1/confluenceMod/modified_files/*.png                               /home/$1/.kodi/addons/skin.confluence/media/
         sudo cp -r /home/$1/confluenceMod/modified_files/plugin.video.drnu                /home/$1/.kodi/addons/
         sudo cp -r /home/$1/confluenceMod/modified_files/service.subtitles.opensubtitles  /home/$1/.kodi/addons/
+        echo '  Fetching DR Addon....'
+	git clone https://github.com/xbmc-danish-addons/plugin.video.drnu /home/$1/.kodi/addons
         echo '     Done!'
         echo
 fi
