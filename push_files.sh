85ff416746f3c48bdb7a9a0cbbb54ed843b88a44
poulkalff <poulkalff@gmail.com>
2025-03-15 10:06:55 +0100

    Added youtube api keys

--- push_files.sh
+++ push_files.sh
@@ -30,8 +30,9 @@ else
 #        sudo cp /home/$1/confluenceMod/modified_files/Includes.xml                        /home/$1/.kodi/addons/skin.confluence/1080p/
         sudo cp /home/$1/confluenceMod/modified_files/advancedsettings.xml                /home/$1/.kodi/userdata/
         sudo cp /home/$1/confluenceMod/modified_files/sources.xml                         /home/$1/.kodi/userdata/
-        sudo cp /home/$1/confluenceMod/modified_files/*.xsp                               /home/$1/.kodi/userdata/playlists/video/
         sudo cp /home/$1/confluenceMod/modified_files/settings.xml                        /home/$1/.kodi/userdata/
+        sudo cp /home/$1/confluenceMod/modified_files/guisettings.xml                     /home/$1/.kodi/userdata/
+        sudo cp /home/$1/confluenceMod/modified_files/*.xsp                               /home/$1/.kodi/userdata/playlists/video/
         sudo cp /home/$1/confluenceMod/modified_files/*.png                               /home/$1/.kodi/addons/skin.confluence/media/
         sudo cp -r /home/$1/confluenceMod/modified_files/plugin.video.drnu                /home/$1/.kodi/addons/
         sudo cp -r /home/$1/confluenceMod/modified_files/service.subtitles.opensubtitles  /home/$1/.kodi/addons/
